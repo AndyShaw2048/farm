a194050f2476524d230eb39b928382e514a232c8
bai <137695234@qq.com>
2018-05-29 20:03:10 +0800

    test

--- app/Http/Controllers/Cart/CartController.php
+++ app/Http/Controllers/Cart/CartController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers\Cart;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Mockery\CountValidator\Exception;
 
 class CartController extends Controller
 {
     public function index()
     {
         return view('home.cart');
+    }
+    
+    public function add(Request $request)
+    {
+        return response()->json(array([
+            'code' => '200'
+                                      ]));
     }
 }
